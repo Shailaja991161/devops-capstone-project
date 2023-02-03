5985ad8ca0afb457623b3d614f59289790ac890e
Shailaja991161 <shailajashah16@gmail.com>
2023-02-03 03:20:20 -0500

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -14,6 +14,7 @@ from flask_talisman import Talisman
 app = Flask(__name__)
 app.config.from_object(config)
 talisman = Talisman(app)
+CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
