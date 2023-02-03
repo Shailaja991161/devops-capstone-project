e4c2700a22da61a1a8f3e70d368f07eda999e33b
Shailaja991161 <shailajashah16@gmail.com>
2023-02-03 01:18:11 -0500

    main

--- service/routes.py
+++ service/routes.py
@@ -105,15 +105,6 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
-######################################################################
-# DELETE AN ACCOUNT
-######################################################################
-
-# ... place you code here to DELETE an account ...
-    ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
